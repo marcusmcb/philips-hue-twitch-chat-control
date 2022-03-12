4f8c36877817b42f96a70cd3ed67bfca7e598ee4
Marcus McBride <marcusmcb@gmail.com>
2022-03-12 15:38:55 -0800

    added rate limiting and options option

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const {
   setLightsToColor,
 } = require('./hueLights')
 
+// global vars to track and prevent command spamming
+let lastCommand
+let lastUser
+let commandCount = 0
+
 // secure twitch oauth token for tmi
 dotenv.config()
 
@@ -36,57 +41,113 @@ client.on('message', (channel, tags, message, self) => {
   }
 
   const args = message.slice(1).split(' ')
-  const command = args.shift().toLowerCase()  
+  const command = args.shift().toLowerCase()
 
-  switch (command) {
-    case 'flarby':
-      client.say(channel, 'WORKING')
-      break
-
-    // commands for hue lights
-    case 'lights':
-      // check for lighting command option
-      if (args.length != 0) {
-        // !lights on
-        if (args == 'on') {
-          turnLightsOnOrOff(true)
-          break
-        }
-        // !lights off
-        if (args == 'off') {
-          turnLightsOnOrOff(false)
-          break
-        }
-        // !lights random
-        if (args == 'random') {
-          setLightsToRandomColors()
-          break
-        }
-        // !lights (color)
-        if (
-          args == 'green' ||
-          'pink' ||
-          'teal' ||
-          'purple' ||
-          'red' ||
-          'gold' ||
-          'blue' ||
-          'peach'
-        ) {
-          setLightsToColor(args)
-          break
-        }
-      } else {
-        // if empty, display options & prompt user to try again
-        client.say(
-          channel,
-          'You can control my lighting with the following options --> on, off, random, green, blue, red, purple, pink, teal, gold, peach.'
-        )
+  const runCommand = (command) => {
+    switch (command) {
+      case 'flarby':
+        client.say(channel, 'WORKING')
         break
-      }
 
-    // no response as default for commands that don't exist
-    default:
-      break
+      // commands for hue lights
+      case 'lights':
+        // check for lighting command option
+        if (args.length != 0) {
+          // !lights on
+          if (args == 'on') {
+            turnLightsOnOrOff(true)
+            break
+          }
+          // !lights off
+          if (args == 'off') {
+            turnLightsOnOrOff(false)
+            break
+          }
+          // !lights random
+          if (args == 'random') {
+            setLightsToRandomColors()
+            break
+          }
+          // !lights options
+          if (args == 'options') {
+            client.say(
+              channel,
+              'You can control my lighting with the following options --> on, off, random, green, blue, red, purple, pink, teal, gold, peach.'
+            )
+          }
+          // !lights (color)
+          if (
+            args == 'green' ||
+            'pink' ||
+            'teal' ||
+            'purple' ||
+            'red' ||
+            'gold' ||
+            'blue' ||
+            'peach'
+          ) {
+            setLightsToColor(args)
+            break
+          }
+        } else {
+          // if empty, display options & prompt user to try again
+          client.say(
+            channel,
+            'You can control my lighting with the following options --> on, off, random, green, blue, red, purple, pink, teal, gold, peach.'
+          )
+          break
+        }
+
+      // no response as default for commands that don't exist
+      default:
+        break
+    }
+  }
+
+  // user is limited to 6 consecutive uses of each command
+  // beyond that cap, user is prompted to use another command
+  const rateLimited = () => {
+    client.say(
+      channel,
+      `${tags.username}, try a different command before using that one again.`
+    )
+  }
+
+  // master list of current commands in this script for our client connection to listen for
+  // any commands added/updated above need to be added/updated here
+  const commandList = ['lights', 'flarby']
+
+  // check if command is in list
+  if (commandList.includes(command)) {
+    // check if the same user has entered the same command consecutively more than once
+    if (lastCommand == command && lastUser == tags.username) {
+      console.log(true)
+      commandCount++
+      console.log('COMMAND COUNT: ', commandCount)
+      // redirect user to another command on rate limit
+      if (commandCount === 6) {
+        rateLimited()
+        // ignore further commands from user if spamming
+      } else if (commandCount > 6) {
+        return
+        // run command otherwise
+      } else {
+        runCommand(command)
+      }
+      // if not, call method/function that runs switch selector, set vars and counter
+    } else {
+      console.log(false)
+      lastCommand = command
+      lastUser = tags.username
+      commandCount = 1
+      runCommand(command)
+    }
+  } else {
+    // if command is not in list, reset count and return w/o response
+    // we only want this script to listen for commands within the commandList
+    // prevents response and rate-limiting conflicts w/other bots configured for the same channel
+    commandCount = 0
+    // reset args
+    return
   }
 })
