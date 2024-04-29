7ce9371db1fa22cdb4f4359ee72746906d38af5b
marcusmcb <marcusmcb@gmail.com>
2024-04-28 21:26:17 -0700

    added candle, fireplace, and traffic light commands and automation

--- hueLights.js
+++ hueLights.js
@@ -1,125 +1,208 @@
 const axios = require('axios')
 
 const ids = [1]
+const lightEffectControl = {}
 
-const turnLightOnOrOff = async (lightId, on, hue, sat, bri, effect) => {  
-  console.log("EFFECT: ", effect)
-  console.log("HUE: ", hue)
-  try {
-    // turns off any color effects present *first*
-    await axios.put(
-      `http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
-      {
-        on,
-        ...(effect && { effect })        
-      }
-    )
-    // ...then sets to fixed color properties
-    await axios.put(
-      `http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
-      {
-        on,        
-        ...(sat && { sat }),
-        ...(bri && { bri }),
-        ...(hue && { hue }),        
-      }
-    )
-  } catch (err) {
-    console.error(err)
-  }
+const turnLightOnOrOff = async (lightId, on, hue, sat, bri, effect) => {
+	console.log('EFFECT: ', effect)
+	console.log('HUE: ', hue)
+	lightEffectControl[lightId] = false
+	try {
+		// turns off any color effects present *first*
+		await axios.put(
+			`http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
+			{
+				on,
+				...(effect && { effect }),
+			}
+		)
+		// ...then sets to fixed color properties
+		await axios.put(
+			`http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
+			{
+				on,
+				...(sat && { sat }),
+				...(bri && { bri }),
+				...(hue && { hue }),
+			}
+		)
+	} catch (err) {
+		console.error(err)
+	}
 }
 
 const turnLightMorphOn = async (lightId, on, hue, sat, bri, effect) => {
-  console.log("EFFECT: ", effect)
-  console.log("HUE: ", hue)
-  try {
-    return await axios.put(
-      `http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
-      {
-        on,
-        ...(sat && { sat }),
-        ...(bri && { bri }),
-        ...(hue && { hue }),
-        ...(effect && { effect }),
-      }
-    )
-  } catch (err) {
-    console.error(err)
-  }
+	console.log('EFFECT: ', effect)
+	console.log('HUE: ', hue)
+	lightEffectControl[lightId] = false
+	try {
+		return await axios.put(
+			`http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
+			{
+				on,
+				...(sat && { sat }),
+				...(bri && { bri }),
+				...(hue && { hue }),
+				...(effect && { effect }),
+			}
+		)
+	} catch (err) {
+		console.error(err)
+	}
 }
 
 const setLightsToMorph = () => {
-  ids.forEach((id) => {
-    const hue = Math.floor(Math.random() * 65535) + 1    
-    const sat = 200
-    const bri = 200
-    const effect = 'colorloop'
-    turnLightMorphOn(id, true, hue, sat, bri, effect)
-  })
+	ids.forEach((id) => {
+		const hue = Math.floor(Math.random() * 65535) + 1
+		const sat = 200
+		const bri = 200
+		const effect = 'colorloop'
+		turnLightMorphOn(id, true, hue, sat, bri, effect)
+	})
 }
 
 const turnLightsOnOrOff = (on) => {
-  ids.forEach((id) => turnLightOnOrOff(id, on))
+	ids.forEach((id) => turnLightOnOrOff(id, on))
 }
 
 const setLightsToRandomColors = () => {
-  ids.forEach((id) => {
-    const hue = Math.floor(Math.random() * 65535) + 1    
-    const sat = 200
-    const bri = 200
-    const effect = 'none'
-    turnLightOnOrOff(id, true, hue, sat, bri, effect)
-  })
+	ids.forEach((id) => {
+		const hue = Math.floor(Math.random() * 65535) + 1
+		const sat = 200
+		const bri = 200
+		const effect = 'none'
+		turnLightOnOrOff(id, true, hue, sat, bri, effect)
+	})
 }
 
 const setLightsToColor = (color) => {
-  let hueValue
-  // console.log('COLOR: ', color[0])
-  // philips hue values for color command options  
-  if (color == 'teal') {
-    hueValue = 31421
-  }
-  if (color == 'pink') {
-    hueValue = 60364
-  }
-  if (color == 'green') {
-    hueValue = 25000
-  }
-  if (color == 'purple') {
-    hueValue = 48500
-  }
-  if (color == 'red') {
-    hueValue = 65000
-  }
-  if (color == 'gold') {
-    hueValue = 11000
-  }
-  if (color == 'blue') {
-    hueValue = 40000
-  }
-  if (color == 'peach') {
-    hueValue = 2500
-  }
+	let hueValue
+	// console.log('COLOR: ', color[0])
+	// philips hue values for color command options
+	if (color == 'teal') {
+		hueValue = 31421
+	}
+	if (color == 'pink') {
+		hueValue = 60364
+	}
+	if (color == 'green') {
+		hueValue = 25000
+	}
+	if (color == 'purple') {
+		hueValue = 48500
+	}
+	if (color == 'red') {
+		hueValue = 65000
+	}
+	if (color == 'gold') {
+		hueValue = 11000
+	}
+	if (color == 'blue') {
+		hueValue = 40000
+	}
+	if (color == 'peach') {
+		hueValue = 2500
+	}
 
-  ids.forEach((id) => {
-    const hue = hueValue
-    const sat = 200
-    const bri = 200
-    const effect = 'none'
-    turnLightOnOrOff(id, true, hue, sat, bri, effect)
-  })
+	ids.forEach((id) => {
+		const hue = hueValue
+		const sat = 200
+		const bri = 200
+		const effect = 'none'
+		turnLightOnOrOff(id, true, hue, sat, bri, effect)
+	})
 }
 
 // requires more than one hue fixture for the effect to display properly
 const setLightsForChristmas = () => {
-  turnLightOnOrOff(ids[0], true, 27306, 150, 175)
-  turnLightOnOrOff(ids[1], true, 1, 150, 175)
+	lightEffectControl[lightId] = false
+	turnLightOnOrOff(ids[0], true, 27306, 150, 175)
+	turnLightOnOrOff(ids[1], true, 1, 150, 175)
+}
+
+const simulateCandle = async (lightId) => {
+	lightEffectControl[lightId] = true // Enable the effect loop
+	const flicker = async () => {
+		if (!lightEffectControl[lightId]) return // Exit if control is disabled
+		const bri = Math.floor(Math.random() * 20) + 200 // Brightness between 200 and 220
+		const ct = Math.floor(Math.random() * 80) + 440 // Color temperature between 440 and 520
+		try {
+			await axios.put(
+				`http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
+				{ on: true, bri, ct }
+			)
+			setTimeout(flicker, 200 + Math.random() * 200)
+		} catch (err) {
+			console.error(err)
+		}
+	}
+	flicker()
+}
+
+const setLightsToCandleEffect = () => {
+	ids.forEach((id) => simulateCandle(id))
+}
+
+const simulateFireplace = async (lightId) => {
+	lightEffectControl[lightId] = true
+	const flicker = async () => {
+		if (!lightEffectControl[lightId]) return // Stop if control is disabled
+		const bri = Math.floor(Math.random() * 64) + 160 // Brightness between 160 and 224
+		const ct = Math.floor(Math.random() * 120) + 400 // Color temperature between 400 and 520
+		try {
+			await axios.put(
+				`http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
+				{ on: true, bri, ct }
+			)
+			setTimeout(flicker, 100 + Math.random() * 150)
+		} catch (err) {
+			console.error(err)
+		}
+	}
+	flicker()
+}
+
+const setLightsToFireplaceEffect = () => {
+	ids.forEach((id) => simulateFireplace(id))
+}
+
+const simulateTrafficLight = async (lightId) => {
+	lightEffectControl[lightId] = true // Enable the effect loop
+	const changeColor = async (hue, duration) => {
+		if (!lightEffectControl[lightId]) return // Exit if control is disabled
+		try {
+			await axios.put(
+				`http://${process.env.HUE_BRIDGE_ADDRESS}/api/${process.env.HUE_AUTH_USER}/lights/${lightId}/state`,
+				{ on: true, bri: 254, hue: hue }
+			)
+		} catch (err) {
+			console.error(err)
+		}
+		return new Promise((resolve) => setTimeout(resolve, duration))
+	}
+
+	const cycleColors = async () => {
+		if (!lightEffectControl[lightId]) return // Exit if control is disabled
+		await changeColor(25500, 5000) // Green for 4 seconds
+		await changeColor(12750, 2000) // Yellow for 1 second
+		await changeColor(0, 5000) // Red for 4 seconds
+		cycleColors() // Repeat the cycle
+	}
+
+	cycleColors()
+}
+
+const setLightsToTrafficLightEffect = () => {
+	ids.forEach((id) => simulateTrafficLight(id))
 }
 
 module.exports = {
-  setLightsForChristmas,
-  setLightsToRandomColors,
-  turnLightsOnOrOff,
-  setLightsToColor,
-  setLightsToMorph
+	setLightsForChristmas,
+	setLightsToRandomColors,
+	turnLightsOnOrOff,
+	setLightsToColor,
+	setLightsToMorph,
+	setLightsToCandleEffect,
+	setLightsToFireplaceEffect,
+  setLightsToTrafficLightEffect
 }
--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const tmi = require('tmi.js')
 const dotenv = require('dotenv')
 
 const {
-  setLightsToRandomColors,
-  turnLightsOnOrOff,
-  setLightsToColor,
-  setLightsToMorph
+	setLightsToRandomColors,
+	turnLightsOnOrOff,
+	setLightsToColor,
+	setLightsToMorph,
+	setLightsToCandleEffect,
+	setLightsToFireplaceEffect,
+	setLightsToTrafficLightEffect
 } = require('./hueLights')
 
 // global vars to track and prevent command spamming
@@ -17,144 +20,158 @@ dotenv.config()
 
 // create tmi instance
 const client = new tmi.Client({
-  options: { debug: true },
-  connection: {
-    secure: true,
-    reconnect: true,
-  },
-  identity: {
-    username: process.env.TWITCH_BOT_USERNAME,
-    password: process.env.TWITCH_OAUTH_TOKEN,
-  },
-  channels: [process.env.TWITCH_CHANNEL_NAME],
+	options: { debug: true },
+	connection: {
+		secure: true,
+		reconnect: true,
+	},
+	identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN,
+	},
+	channels: [process.env.TWITCH_CHANNEL_NAME],
 })
 
 client.connect()
 
 // chat command listener
 client.on('message', (channel, tags, message, self) => {
-  console.log('MESSAGE: ', message)
-  if (self || !message.startsWith('!')) {
-    return
-  }
+	console.log('MESSAGE: ', message)
+	if (self || !message.startsWith('!')) {
+		return
+	}
 
-  const args = message.slice(1).split(' ')
-  const command = args.shift().toLowerCase()
-  const channelName = channel.slice(1).split('#')
+	const args = message.slice(1).split(' ')
+	const command = args.shift().toLowerCase()
+	const channelName = channel.slice(1).split('#')
 
-  const runCommand = (command) => {
-    switch (command) {
-      case 'lights-test':
-        client.say(channel, `Your script is successfully connected to ${channelName}'s channel.`)
-        break
+	const runCommand = (command) => {
+		switch (command) {
+			case 'lights-test':
+				client.say(
+					channel,
+					`Your script is successfully connected to ${channelName}'s channel.`
+				)
+				break
+			case 'fireplace':
+				console.log("Fireplace Effect")
+				setLightsToFireplaceEffect()
+				break
+			case 'candle':
+				console.log("Candle Effect")
+				setLightsToCandleEffect()
+				break
+			case 'traffic':
+				console.log("Traffic Light Effect")
+				setLightsToTrafficLightEffect()
+				break
+			// commands for hue lights
+			case 'lights':
+				// check for lighting command option
+				if (args.length != 0) {
+					// !lights on
+					if (args == 'on') {
+						turnLightsOnOrOff(true)
+						break
+					}
+					// !lights off
+					if (args == 'off') {
+						turnLightsOnOrOff(false)
+						break
+					}
+					// !lights random
+					if (args == 'random') {
+						setLightsToRandomColors()
+						break
+					}
+					// !lights options
+					if (args == 'options') {
+						client.say(
+							channel,
+							'You can control my lighting with the following options --> on, off, random, green, blue, red, purple, pink, teal, gold, peach, morph.'
+						)
+						break
+					}
+					// !lights morph
+					if (args == 'morph') {
+						setLightsToMorph()
+						break
+					}
+					// !lights (color)
+					if (
+						args == 'green' ||
+						'pink' ||
+						'teal' ||
+						'purple' ||
+						'red' ||
+						'gold' ||
+						'blue' ||
+						'peach'
+					) {
+						setLightsToColor(args)
+						break
+					}
+				} else {
+					// if empty, display options & prompt user to try again
+					client.say(
+						channel,
+						'You can control my lighting with the following options --> on, off, random, green, blue, red, purple, pink, teal, gold, peach, morph.'
+					)
+					break
+				}
 
-      // commands for hue lights
-      case 'lights':
-        // check for lighting command option
-        if (args.length != 0) {
-          // !lights on
-          if (args == 'on') {
-            turnLightsOnOrOff(true)
-            break
-          }
-          // !lights off
-          if (args == 'off') {
-            turnLightsOnOrOff(false)
-            break
-          }
-          // !lights random
-          if (args == 'random') {
-            setLightsToRandomColors()
-            break
-          }
-          // !lights options
-          if (args == 'options') {
-            client.say(
-              channel,
-              'You can control my lighting with the following options --> on, off, random, green, blue, red, purple, pink, teal, gold, peach, morph.'
-            )
-            break
-          }
-          // !lights morph
-          if (args == 'morph') {
-            setLightsToMorph()
-            break
-          }
-          // !lights (color)
-          if (
-            args == 'green' ||
-            'pink' ||
-            'teal' ||
-            'purple' ||
-            'red' ||
-            'gold' ||
-            'blue' ||
-            'peach'
-          ) {
-            setLightsToColor(args)
-            break
-          }
-        } else {
-          // if empty, display options & prompt user to try again
-          client.say(
-            channel,
-            'You can control my lighting with the following options --> on, off, random, green, blue, red, purple, pink, teal, gold, peach, morph.'
-          )
-          break
-        }
+			// no response as default for commands that don't exist
+			default:
+				break
+		}
+	}
 
-      // no response as default for commands that don't exist
-      default:
-        break
-    }
-  }
+	// user is limited to 6 consecutive uses of each command
+	// beyond that cap, user is prompted to use another command
+	const rateLimited = () => {
+		client.say(
+			channel,
+			`${tags.username}, try a different command before using that one again.`
+		)
+	}
 
-  // user is limited to 6 consecutive uses of each command
-  // beyond that cap, user is prompted to use another command
-  const rateLimited = () => {
-    client.say(
-      channel,
-      `${tags.username}, try a different command before using that one again.`
-    )
-  }
+	// master list of current commands in this script for our client connection to listen for
+	// any commands added/updated above need to be added/updated here
+	const commandList = ['lights', 'lights-test', 'candle', 'fireplace', 'traffic']
 
-  // master list of current commands in this script for our client connection to listen for
-  // any commands added/updated above need to be added/updated here
-  const commandList = ['lights', 'lights-test']
-
-  // check if command is in list
-  if (commandList.includes(command)) {
-    // check if the same user has entered the same command consecutively more than once
-    if (lastCommand == command && lastUser == tags.username) {
-      console.log(true)
-      commandCount++
-      console.log('COMMAND COUNT: ', commandCount)
-      // redirect user to another command on rate limit
-      if (commandCount === 6) {
-        rateLimited()
-        // ignore further commands from user if spamming
-      } else if (commandCount > 6) {
-        return
-        // run command otherwise
-      } else {
-        runCommand(command)
-      }
-      // if not, call method/function that runs switch selector, set vars and counter
-    } else {
-      console.log(false)
-      lastCommand = command
-      lastUser = tags.username
-      commandCount = 1
-      runCommand(command)
-    }
-  } else {
-    // if command is not in list, reset count and return w/o response
-    // we only want this script to listen for commands within the commandList
-    // prevents response and rate-limiting conflicts w/other bots configured for the same channel
-    commandCount = 0
-    // reset args
-    return
-  }
+	// check if command is in list
+	if (commandList.includes(command)) {
+		// check if the same user has entered the same command consecutively more than once
+		if (lastCommand == command && lastUser == tags.username) {
+			console.log(true)
+			commandCount++
+			console.log('COMMAND COUNT: ', commandCount)
+			// redirect user to another command on rate limit
+			if (commandCount === 6) {
+				rateLimited()
+				// ignore further commands from user if spamming
+			} else if (commandCount > 6) {
+				return
+				// run command otherwise
+			} else {
+				runCommand(command)
+			}
+			// if not, call method/function that runs switch selector, set vars and counter
+		} else {
+			console.log(false)
+			lastCommand = command
+			lastUser = tags.username
+			commandCount = 1
+			runCommand(command)
+		}
+	} else {
+		// if command is not in list, reset count and return w/o response
+		// we only want this script to listen for commands within the commandList
+		// prevents response and rate-limiting conflicts w/other bots configured for the same channel
+		commandCount = 0
+		// reset args
+		return
+	}
 })
 
 // reference postman collections for PUT requests to change light params
